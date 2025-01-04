ba4cc653b41bf29f5c7e50c8e04c4f49abab9d56
asivery <asivery@protonmail.com>
2025-01-04 03:54:34 +0100

    Include @hasechris' script for rebuilding the hashtab, clarify some points in the readme

--- scripts/install_script.sh
+++ scripts/install_script.sh
@@ -32,11 +32,59 @@ systemctl daemon-reload
 systemctl restart xochitl
 EOF
 
+cat << EOF > $XOVI_INSTALL_DIR/rebuild_hashtable
+#!/bin/bash
+
+if [[ ! -e '/home/root/xovi/extensions.d/qt-resource-rebuilder.so' ]]; then
+    echo "Please install qt-resource-rebuilder before updating the hashtable"
+    exit 1
+fi
+
+# stop systemwide gui process
+systemctl stop xochitl.service
+
+if pidof xochitl; then
+  kill -15 \$(pidof xochitl)
+fi
+
+# remove the actual hashtable
+rm -f /home/root/xovi/exthome/qt-resource-rebuilder/hashtab
+
+# start update hashtab process
+echo -e "#################################"
+echo -e "Now we will run the GUI process with the arguments to build a new hashtable."
+echo -e "\n\t[...]"
+echo -e "\t[qmldiff] [Hashtab Rule Processor]: Hashed derived '...'"
+echo -e "\t[qmldiff]: Hashtab saved to /home/root/xovi/exthome/qt-resource-rebuilder/hashtab"
+echo -e "\nWhen we find this line in the output the process is stopped and the systemd service is started again."
+echo -e "\nPlease enter your password on the rM when prompted."
+read -p "Please press enter to continue:"
+
+echo -e "\n\nOutput:"
+sleep 3
+
+QMLDIFF_HASHTAB_CREATE=/home/root/xovi/exthome/qt-resource-rebuilder/hashtab QML_DISABLE_DISK_CACHE=1 LD_PRELOAD=/home/root/xovi/xovi.so /usr/bin/xochitl 2>&1 | while IFS= read line; do
+  echo -e "\$line"
+  if [[ "\$line" == "[qmldiff]: Hashtab saved to /home/root/xovi/exthome/qt-resource-rebuilder/hashtab" ]]; then
+    # yep, found the line we are wating for. exiting the process.
+    echo -e "\n##############"
+    echo -e "Found expected output. Killing gui process and restarting systemd service."
+    kill -15 \$(pidof xochitl)
+  fi
+done
+
+# so, we seem to be finished.
+# wait another 5 seconds, then run the normal gui process via systemd
+sleep 5
+echo -e "Now starting the systemd xochitl service back up..."
+systemctl start xochitl.service
+EOF
+
 echo "Extracting dlfile..."
 cd $XOVI_INSTALL_DIR
 PAYLOAD_LINE=$(awk '/^__PAYLOAD__/ { print NR + 1; exit 0; }' $0)
 tail -n +$PAYLOAD_LINE $0 | gzip -d > dlfile
-chmod a+x dlfile start debug stock
+chmod a+x dlfile start debug stock rebuild_hashtable
 
 echo "Downloading xovi..."
 ./dlfile "https://github.com/asivery/xovi/releases/latest/download/xovi.so"
