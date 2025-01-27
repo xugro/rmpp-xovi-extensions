855edc12957d7ac6440d69e789be8dfd79b7ac74
xugro <82907135+xugro@users.noreply.github.com>
2025-01-27 17:30:05 +0000

    make build_all.sh just run make on project root

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -8,14 +8,9 @@ mkdir -p $TMP
 mkdir -p "$XOVI_REPO/util"
 curl -o "$XOVI_REPO/util/xovigen.py" https://raw.githubusercontent.com/asivery/xovi/1edb3c9f0a31daec31b6931eb00f4f6ecc56d72b/util/xovigen.py
 
-for dir in ../*; do
-    if [ -d $dir ] && [ $dir != "../scripts" ]; then
-        echo "Building from $dir..."
-        cd "$dir"
-        bash make-aarch64.sh
-        cd "$SCR_ROOT"
-    fi
-done
+
+source ~/Tools/remarkable-toolchain/environment-setup-cortexa53-crypto-remarkable-linux
+make -C ..
 
 
 mkdir output
